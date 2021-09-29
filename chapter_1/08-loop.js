c153f4c56fef6cce8f8050ad0b050e6d58d8b0de
Otavio Sousa <otavioklearskin@gmail.com>
2021-09-29 18:57:36 -0300

    loop

---
+++ chapter_1/08-loop.js
@@ -0,0 +1,15 @@
+for(var i = 0; i < 10; i++){
+    console.log(i)
+}
+
+var i = 0
+while(i < 10) {
+    console.log(i)
+    i++
+}
+
+var count = 0
+do {
+    console.log(count)
+    count++
+} while(count < 10)
