900143292dbd82075834848a886628cd8b7aae5b
Otavio Sousa <otavioklearskin@gmail.com>
2021-10-02 13:49:55 -0300

    let const

---
+++ chapter_2/11-let-const.js
@@ -0,0 +1,23 @@
+var framwork = 'React'
+var framwork = 'Angular' // declare two var same name
+console.log(framwork)
+
+let language = 'Js'
+// let language = 'Python' error
+console.log(language)
+
+const PI = 3.14
+// PI = 2.14 error
+// var PI = 2.14 error
+// let PI = 2.14 error
+console.log(PI)
+
+const jsFramwork = {
+    name: 'React'
+}
+console.log(jsFramwork.name)
+jsFramwork.name = 'Vue'
+console.log(jsFramwork.name)
+// jsFramwork = {
+//     name: 'Angular'
+// }  error
