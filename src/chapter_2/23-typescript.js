6030d89ce9b5e935fc4d84063025e2b3cefb7c6f
Otavio Sousa <otavioklearskin@gmail.com>
2021-10-08 17:28:29 -0300

    typescript

---
+++ src/chapter_2/23-typescript.js
@@ -0,0 +1,4 @@
+// tsc src/chapter_2/23-typescript.ts
+var msg = 'Hello world';
+msg = 10;
+console.log(msg);
---
+++ src/chapter_2/23-typescript.ts
@@ -0,0 +1,5 @@
+// tsc src/chapter_2/23-typescript.ts
+
+let msg = 'Hello world'
+msg = 10
+console.log(msg)
